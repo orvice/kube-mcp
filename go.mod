5a9089776f6bd3d6652fe93baab0ec899462ecb4
orvice L <orvice@gmail.com>
2025-03-02 10:43:45 +0800

    add namespace,ingress,deployment

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- internal/kube/tool.go
+++ internal/kube/tool.go
@@ -13,5 +13,20 @@ var (
 			Description: "List pods in the current namespace",
 			Handler:     ListPods,
 		},
+		{
+			Name:        "list_deployments",
+			Description: "List deployments in the current namespace",
+			Handler:     ListDeployments,
+		},
+		{
+			Name:        "list_ingresses",
+			Description: "List ingresses in the current namespace",
+			Handler:     ListIngresses,
+		},
+		{
+			Name:        "list_namespaces",
+			Description: "List all namespaces in the cluster",
+			Handler:     ListNamespaces,
+		},
 	}
 )
